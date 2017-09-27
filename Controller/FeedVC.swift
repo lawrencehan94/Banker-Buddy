209809517f05889fce2856e12fa09a3011fdf38c
Lawrence <lawrencehan94@gmail.com>
2017-09-27 13:24:55 -0700

    v2

--- Controller/FeedVC.swift
+++ Controller/FeedVC.swift
@@ -1,6 +1,10 @@
 import UIKit
 import CoreData
 
+//APP DELEGATE
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
+
+//FEED CLASS
 class FeedVC {
   
   var deals = [Deal]()
@@ -34,11 +38,27 @@ extension FeedVC: UITableViewDelegate, UITableViewDataSource {
 
 //CORE DATA
 func fetch(completion: (_ complete: Bool) -> ()) {
-
+  guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+  let fetchRequest = NSFetchRequest<Deal>(entityName: "Deal")
+  
+  do {
+    deals = try managedContext.fetch(fetchRequest)
+    completion(true)
+  } catch {
+    debugPrint("Could not fetch: \(error.localizedDescription)")
+    completion(false)
+  }
 }
 
 
-//NOTES
+//GENERAL NOTES
 // A managed object context is a scratchpad where you can work with your managed objects (or the objects you want to save)
 // My function, "fetch" is going to return a completion variable (inside a completion handler) that is either true or false. 
 // This will allow whomever is fetching the data through the "fetch" function to set  what will happen if completion is true or false
+
+//NOTES ON APP DELEGATE
+// An app delegate sits in the middle of everything - it's the entry point to your application
+// iOS (user presses volume up button or pause button), then app delegate goes to navigation controller then changes the view / something in the model
+// an appdelegate is a singleton (there's only one of it), and is guaranteed to be there (unlike VCs, which can be destroyed by the system to save memory)
+// appdelegate tells vc to pause / play
+// push notification goes to app delegate then goes to function that does something (plays music, etc.)
