158eda7ae60e96a3fc2dd258ad2639fe9024f645
Lawrence <lawrencehan94@gmail.com>
2017-09-27 12:51:15 -0700

    v2

--- Views/FeedCell.swift
+++ Views/FeedCell.swift
@@ -2,14 +2,18 @@ import UIKit
 
 class FeedCell: UITableViewCell {
 
-  @IBOutlet weak var dealLabel: String?
-  @IBOutlet weak var enterpriseValueLabel: String?
-  @IBOutlet weak var LTMRevenueMultipleLabel: String?
+  @IBOutlet weak var dealLabel: String!
+  @IBOutlet weak var enterpriseValueLabel: String!
+  @IBOutlet weak var LTMRevenueMultipleLabel: String!
   
   func configureCell(deal: String, enterpriseValue: Double, LTMRevenueMultiple: Double) {
     self.dealLabel.text = deal
     self.enterpriseValueLabel.text = String(enterpriseValue)
     self.LTMRevenueMultipleLabel.text = String(LTMRevenueMultiple)
   }
+  
+  // Instead of making the parameters deal, enterpriseValue, LTMRevenueMultiple, make it the coredata entity that you created
+  // func configureCell(deal: Deal) 
+  // Here the parameter is of type Deal (the entity that you created with deal, enterpriseValue, and LTMRevenueMultiple properties)
 
 }
